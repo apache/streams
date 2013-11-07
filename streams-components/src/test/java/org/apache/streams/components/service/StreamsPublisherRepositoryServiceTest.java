3da17a38a47ebf8acf20ffbc9c82103dca3fa651
dsullivan <dsullivan@unknown>
2013-11-07 20:14:01 +0000

    removing unneeded imports
    git-svn-id: https://svn.apache.org/repos/asf/incubator/streams/branches/webservice@1539791 13f79535-47bb-0310-9956-ffa450edef68

--- streams-components/src/test/java/org/apache/streams/components/service/StreamsPublisherRepositoryServiceTest.java
+++ streams-components/src/test/java/org/apache/streams/components/service/StreamsPublisherRepositoryServiceTest.java
@@ -8,10 +8,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.easymock.EasyMock.*;
-import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.sameInstance;
-import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
