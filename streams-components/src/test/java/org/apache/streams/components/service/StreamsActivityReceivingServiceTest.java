b9a6b811e448213dd6fb80d8bec088803f0a28dd
dsullivan <dsullivan@unknown>
2013-11-07 18:29:11 +0000

    adding StreamsActivityReceivingServiceTest
    git-svn-id: https://svn.apache.org/repos/asf/incubator/streams/branches/webservice@1539747 13f79535-47bb-0310-9956-ffa450edef68

---
+++ streams-components/src/test/java/org/apache/streams/components/service/StreamsActivityReceivingServiceTest.java
@@ -0,0 +1,34 @@
+package org.apache.streams.components.service;
+
+import org.apache.streams.components.activitysubscriber.ActivityStreamsSubscriberWarehouse;
+import org.apache.streams.components.service.impl.StreamsActivityReceivingServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.easymock.EasyMock.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class StreamsActivityReceivingServiceTest {
+    private StreamsActivityReceivingService acitivityReceivingService;
+    private ActivityStreamsSubscriberWarehouse activityStreamsSubscriberWarehouse;
+
+    @Before
+    public void setup(){
+        activityStreamsSubscriberWarehouse = createMock(ActivityStreamsSubscriberWarehouse.class);
+
+        acitivityReceivingService = new StreamsActivityReceivingServiceImpl(activityStreamsSubscriberWarehouse);
+    }
+
+    @Test
+    public void getActivityTest(){
+        String inRoute = "my inroute";
+        String stream = "hey look, it's a stream";
+
+        expect(activityStreamsSubscriberWarehouse.getStream(inRoute)).andReturn(stream);
+        replay(activityStreamsSubscriberWarehouse);
+
+        assertThat(stream, is(equalTo(acitivityReceivingService.getActivity(inRoute))));
+    }
+}
