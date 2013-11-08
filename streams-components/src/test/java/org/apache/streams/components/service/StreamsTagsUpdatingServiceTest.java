782dfe394af141c0a55dc4206c1d8b7b9ac87681
dsullivan <dsullivan@unknown>
2013-11-08 15:32:41 +0000

    adding StreamsTagsUpdatingServiceTest
    git-svn-id: https://svn.apache.org/repos/asf/incubator/streams/branches/webservice@1540081 13f79535-47bb-0310-9956-ffa450edef68

---
+++ streams-components/src/test/java/org/apache/streams/components/service/StreamsTagsUpdatingServiceTest.java
@@ -0,0 +1,43 @@
+package org.apache.streams.components.service;
+
+import org.apache.streams.components.activitysubscriber.ActivityStreamsSubscriberWarehouse;
+import org.apache.streams.components.service.impl.StreamsTagsUpdatingServiceImpl;
+import org.apache.streams.persistence.model.ActivityStreamsSubscription;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Set;
+
+import static org.easymock.EasyMock.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+public class StreamsTagsUpdatingServiceTest {
+
+    private StreamsTagsUpdatingService tagsUpdatingService;
+    private StreamsSubscriptionRepositoryService repositoryService;
+    private ActivityStreamsSubscriberWarehouse subscriberWarehouse;
+
+    @Before
+    public void setup(){
+        repositoryService = createMock(StreamsSubscriptionRepositoryService.class);
+        subscriberWarehouse = createMock(ActivityStreamsSubscriberWarehouse.class);
+        tagsUpdatingService = new StreamsTagsUpdatingServiceImpl(repositoryService, subscriberWarehouse);
+        }
+
+    @Test
+    public void updateTagsTest() throws Exception {
+        String subscriberId = "subscriberId";
+        String tagsJson = "{\"add\":[\"this\"], \"remove\":[\"that\"]}";
+        ActivityStreamsSubscription subscription = createMock(ActivityStreamsSubscription.class);
+        repositoryService.updateTags(subscriberId, isA(Set.class), isA(Set.class));
+        expectLastCall();
+        subscriberWarehouse.updateSubscriber(subscription);
+        expectLastCall();
+        expect(repositoryService.getSubscriptionByInRoute(subscriberId)).andReturn(subscription);
+
+        String returned = tagsUpdatingService.updateTags(subscriberId,tagsJson);
+
+        assertThat(returned,is(equalTo("Tags Updated Successfully!")));
+    }
+}
