42aadacbc41812d34753285152e4865e0325dfe9
Matthew Hager <Matthew.Hager@gmail.com>
2014-04-15 03:05:49 -0500

    re-worked bulkIndex and updated name validator.

--- streams-util/src/main/java/org/apache/streams/util/SerializationUtil.java
+++ streams-util/src/main/java/org/apache/streams/util/SerializationUtil.java
@@ -1,11 +1,16 @@
 package org.apache.streams.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.*;
 
 /**
  * Created by rebanks on 2/18/14.
  */
 public class SerializationUtil {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SerializationUtil.class);
 
     /**
      * BORROwED FROM APACHE STORM PROJECT
@@ -20,6 +25,8 @@ public class SerializationUtil {
             oos.close();
             return bos.toByteArray();
         } catch(IOException ioe) {
+            LOGGER.error("Unable to Serialize Object: {}", obj.getClass().getName());
+            ioe.printStackTrace();
             throw new RuntimeException(ioe);
         }
     }
