1ac62d7e960b92541dde6864387999893f0d21f2
Robert Douglas <rdouglas@w2odigital.com>
2014-04-25 15:00:33 -0500

    STREAMS-62 | Moved license headers to correct locations

--- streams-contrib/streams-processor-tika/src/test/java/org/apache/streams/tika/TestTikaProcessor.java
+++ streams-contrib/streams-processor-tika/src/test/java/org/apache/streams/tika/TestTikaProcessor.java
@@ -1,9 +1,3 @@
-package org.apache.streams.tika;
-
-import org.apache.commons.lang3.SerializationUtils;
-import org.junit.Test;
-
-
 /*Licensed to the Apache Software Foundation (ASF) under one
 or more contributor license agreements.  See the NOTICE file
 distributed with this work for additional information
@@ -20,6 +14,11 @@ software distributed under the License is distributed on an
 KIND, either express or implied.  See the License for the
 specific language governing permissions and limitations
 under the License.*/
+
+package org.apache.streams.tika;
+
+import org.apache.commons.lang3.SerializationUtils;
+import org.junit.Test;
 
 public class TestTikaProcessor {
     @Test
