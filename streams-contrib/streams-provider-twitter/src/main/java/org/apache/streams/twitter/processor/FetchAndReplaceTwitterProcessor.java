27c6d65da561665e4bf84f6dfd164410027c85c0
mfranklin <mfranklin@apache.org>
2014-05-26 23:34:45 -0400

    Added processor that queries the twitter API

---
+++ streams-contrib/streams-provider-twitter/src/main/java/org/apache/streams/twitter/processor/FetchAndReplaceTwitterProcessor.java
@@ -0,0 +1,141 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.streams.twitter.processor;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
+import org.apache.streams.config.StreamsConfigurator;
+import org.apache.streams.core.StreamsDatum;
+import org.apache.streams.core.StreamsProcessor;
+import org.apache.streams.exceptions.ActivitySerializerException;
+import org.apache.streams.pojo.json.Activity;
+import org.apache.streams.twitter.TwitterStreamConfiguration;
+import org.apache.streams.twitter.pojo.Delete;
+import org.apache.streams.twitter.pojo.Retweet;
+import org.apache.streams.twitter.pojo.Tweet;
+import org.apache.streams.twitter.provider.TwitterEventClassifier;
+import org.apache.streams.twitter.provider.TwitterStreamConfigurator;
+import org.apache.streams.twitter.serializer.StreamsTwitterMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import twitter4j.*;
+import twitter4j.conf.ConfigurationBuilder;
+
+import java.util.List;
+
+import static org.apache.streams.twitter.serializer.util.TwitterActivityUtil.*;
+
+/**
+ *  Given an Activity, fetches the tweet by the activity object id and replaces the existing activity with the converted activity
+ *  from what is returned by the twitter API.
+ */
+public class FetchAndReplaceTwitterProcessor implements StreamsProcessor {
+
+    private static final String PROVIDER_ID = getProvider().getId();
+    private static final Logger LOGGER = LoggerFactory.getLogger(FetchAndReplaceTwitterProcessor.class);
+
+    private final TwitterStreamConfiguration config;
+    private Twitter client;
+    private ObjectMapper mapper;
+
+    public FetchAndReplaceTwitterProcessor() {
+        this(TwitterStreamConfigurator.detectConfiguration(StreamsConfigurator.config.getConfig("twitter")));
+    }
+
+    public FetchAndReplaceTwitterProcessor(TwitterStreamConfiguration config) {
+        this.config = config;
+    }
+
+    @Override
+    public List<StreamsDatum> process(StreamsDatum entry) {
+        if(entry.getDocument() instanceof Activity) {
+            Activity doc = (Activity)entry.getDocument();
+            String originalId = doc.getId();
+            if(PROVIDER_ID.equals(doc.getProvider().getId())) {
+                fetchAndReplace(doc, originalId);
+            }
+        } else {
+            throw new IllegalStateException("Requires an activity document");
+        }
+        return Lists.newArrayList(entry);
+    }
+
+
+    @Override
+    public void prepare(Object configurationObject) {
+        this.client = getTwitterClient();
+        this.mapper = StreamsTwitterMapper.getInstance();
+    }
+
+    @Override
+    public void cleanUp() {
+
+    }  protected void fetchAndReplace(Activity doc, String originalId) {
+        try {
+            String json = fetch(doc);
+            replace(doc, json);
+            doc.setId(originalId);
+        } catch (Exception e) {
+            LOGGER.warn("Error fetching and replacing tweet for activity {}", doc.getId());
+        }
+    }
+
+    protected void replace(Activity doc, String json) throws java.io.IOException, ActivitySerializerException {
+        Class documentSubType = TwitterEventClassifier.detectClass(json);
+        Object object = mapper.readValue(json, documentSubType);
+
+        if(documentSubType.equals(Retweet.class) || documentSubType.equals(Tweet.class)) {
+            updateActivity((Tweet)object, doc);
+        } else if(documentSubType.equals(Delete.class)) {
+            updateActivity((Delete)object, doc);
+        } else {
+            LOGGER.info("Could not determine the correct update method for {}", documentSubType);
+        }
+    }
+
+    protected String fetch(Activity doc) throws TwitterException {
+        String id = doc.getObject().getId();
+        LOGGER.debug("Fetching status from Twitter for {}", id);
+        Long tweetId = Long.valueOf(id.replace("id:twitter:tweets:", ""));
+        Status status = client.showStatus(tweetId);
+        return TwitterObjectFactory.getRawJSON(status);
+    }
+
+
+    protected Twitter getTwitterClient()
+    {
+        String baseUrl = "https://api.twitter.com:443/1.1/";
+
+        ConfigurationBuilder builder = new ConfigurationBuilder()
+                .setOAuthConsumerKey(config.getOauth().getConsumerKey())
+                .setOAuthConsumerSecret(config.getOauth().getConsumerSecret())
+                .setOAuthAccessToken(config.getOauth().getAccessToken())
+                .setOAuthAccessTokenSecret(config.getOauth().getAccessTokenSecret())
+                .setIncludeEntitiesEnabled(true)
+                .setJSONStoreEnabled(true)
+                .setAsyncNumThreads(1)
+                .setRestBaseURL(baseUrl)
+                .setIncludeMyRetweetEnabled(Boolean.TRUE)
+                .setPrettyDebugEnabled(Boolean.TRUE);
+
+        Twitter instance = new TwitterFactory(builder.build()).getInstance();
+        return instance;
+    }
+}
