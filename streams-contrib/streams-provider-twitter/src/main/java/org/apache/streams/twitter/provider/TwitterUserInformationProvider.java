ec2ae35183465a7e309ae6bc243749a25892c837
sblackmon <sblackmon@w2odigital.com>
2014-06-08 22:30:19 -0500

    106-sblackmon

--- streams-contrib/streams-provider-twitter/src/main/java/org/apache/streams/twitter/provider/TwitterUserInformationProvider.java
+++ streams-contrib/streams-provider-twitter/src/main/java/org/apache/streams/twitter/provider/TwitterUserInformationProvider.java
@@ -15,6 +15,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package org.apache.streams.twitter.provider;
 
 import com.google.common.base.Preconditions;
