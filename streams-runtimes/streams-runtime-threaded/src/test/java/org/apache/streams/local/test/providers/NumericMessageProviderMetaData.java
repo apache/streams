80cff488408f31ab772d3bcb89b971add9fb9a01
Matthew Hager <Matthew.Hager@gmail.com>
2014-06-11 11:06:31 -0500

    Added a new provider for metadata and created a new test.

--- streams-runtimes/streams-runtime-threaded/src/test/java/org/apache/streams/local/test/providers/NumericMessageProviderMetaData.java
+++ streams-runtimes/streams-runtime-threaded/src/test/java/org/apache/streams/local/test/providers/NumericMessageProviderMetaData.java
@@ -1,20 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 package org.apache.streams.local.test.providers;
 
 import org.apache.streams.core.StreamsDatum;
@@ -24,23 +7,23 @@ import org.apache.streams.util.ComponentUtils;
 import org.joda.time.DateTime;
 
 import java.math.BigInteger;
-import java.util.Iterator;
+import java.util.HashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-/**
- * Test StreamsProvider that sends out StreamsDatums numbered from 0 to numMessages.
- */
-public class NumericMessageProvider implements StreamsProvider {
-
+public class NumericMessageProviderMetaData implements StreamsProvider {
     protected int startNumber = 0;
     protected int numMessages;
 
-    public NumericMessageProvider() {this.numMessages = 0;}
-    public NumericMessageProvider(int numMessages) {
+
+    public NumericMessageProviderMetaData() {
+        this.numMessages = 0;
+    }
+
+    public NumericMessageProviderMetaData(int numMessages) {
         this.numMessages = numMessages;
     }
 
-    public NumericMessageProvider(int startNumber, int numMessages) {
+    public NumericMessageProviderMetaData(int startNumber, int numMessages) {
         this.startNumber = startNumber;
         this.numMessages = numMessages;
     }
@@ -74,10 +57,17 @@ public class NumericMessageProvider implements StreamsProvider {
         private ResultSet() {
             super(new ConcurrentLinkedQueue<StreamsDatum>());
             for(int i = 0; i < numMessages; i++) {
+                final Integer sequence = i;
+                final String className = this.getClass().getCanonicalName();
                 StreamsDatum datum = new StreamsDatum(new NumericMessageObject(startNumber + i));
+                datum.setMetadata(new HashMap<String, Object>() {{
+                    put("sequence", sequence);
+                    put("class", className);
+                }});
                 ComponentUtils.offerUntilSuccess(datum, this.getQueue());
-
             }
         }
     }
 }
+
+
