933060e42add5b3a640c78a34457e86014a591a5
Steve Blackmon <sblackmon@w2odigital.com>
2014-09-16 11:22:36 -0500

    license / javadoc headers

--- streams-contrib/streams-persist-elasticsearch/src/main/java/org/apache/streams/elasticsearch/processor/DatumFromMetadataAsDocumentProcessor.java
+++ streams-contrib/streams-persist-elasticsearch/src/main/java/org/apache/streams/elasticsearch/processor/DatumFromMetadataAsDocumentProcessor.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.streams.elasticsearch.processor;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -25,7 +43,7 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Created by sblackmon on 9/4/14.
+ * Uses index and type in metadata map stored in datum document to populate current document into datums
  */
 public class DatumFromMetadataAsDocumentProcessor implements StreamsProcessor, Serializable {
 
--- streams-contrib/streams-persist-elasticsearch/src/main/java/org/apache/streams/elasticsearch/processor/DatumFromMetadataProcessor.java
+++ streams-contrib/streams-persist-elasticsearch/src/main/java/org/apache/streams/elasticsearch/processor/DatumFromMetadataProcessor.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.streams.elasticsearch.processor;
 
 import com.google.common.collect.Lists;
@@ -16,7 +34,7 @@ import java.io.Serializable;
 import java.util.List;
 
 /**
- * Created by sblackmon on 9/4/14.
+ * Uses index and type in metadata to populate current document into datums
  */
 public class DatumFromMetadataProcessor implements StreamsProcessor, Serializable {
 
