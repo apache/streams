42d0ab33f20cada4bbf71eb590a4858a1f1cad2e
Robert Douglas <rdouglas@w2ogroup.com>
2014-10-28 12:03:57 -0500

    STREAMS-202 | Additional modifications

--- streams-contrib/streams-provider-google/google-gplus/src/main/java/com/google/gplus/processor/GooglePlusTypeConverter.java
+++ streams-contrib/streams-provider-google/google-gplus/src/main/java/com/google/gplus/processor/GooglePlusTypeConverter.java
@@ -1,7 +1,91 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.google.gplus.processor;
 
-/**
- * Created by rdouglas on 10/27/14.
- */
-public class GooglePlusTypeConverter {
+import com.google.api.services.plus.model.Person;
+import com.google.common.collect.Lists;
+import com.google.gplus.serializer.util.GooglePlusActivityUtil;
+import org.apache.streams.core.StreamsDatum;
+import org.apache.streams.core.StreamsProcessor;
+import org.apache.streams.pojo.json.Activity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Queue;
+
+public class GooglePlusTypeConverter implements StreamsProcessor {
+    public final static String STREAMS_ID = "GooglePlusTypeConverter";
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(GooglePlusTypeConverter.class);
+    private Queue<Person> inQueue;
+    private Queue<StreamsDatum> outQueue;
+    private GooglePlusActivityUtil googlePlusActivityUtil;
+    private int count = 0;
+
+    public GooglePlusTypeConverter() {}
+
+    public Queue<StreamsDatum> getProcessorOutputQueue() {
+        return outQueue;
+    }
+
+    public void setProcessorInputQueue(Queue<Person> inputQueue) {
+        inQueue = inputQueue;
+    }
+
+    @Override
+    public List<StreamsDatum> process(StreamsDatum entry) {
+        StreamsDatum result = null;
+
+        try {
+            Object item = entry.getDocument();
+
+            LOGGER.debug("{} processing {}", STREAMS_ID, item.getClass());
+            Activity activity = null;
+            if(item instanceof Person) {
+                activity = new Activity();
+
+                googlePlusActivityUtil.updateActivity((Person)item, activity);
+            }
+
+            if(activity != null) {
+                result = new StreamsDatum(activity);
+                count++;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOGGER.error("Exception while converting Person to Activity: {}", e.getMessage());
+        }
+
+        if( result != null )
+            return Lists.newArrayList(result);
+        else
+            return Lists.newArrayList();
+    }
+
+    @Override
+    public void prepare(Object configurationObject) {
+        googlePlusActivityUtil = new GooglePlusActivityUtil();
+    }
+
+    @Override
+    public void cleanUp() {
+        //No-op
+    }
 }
--- streams-contrib/streams-provider-google/google-gplus/src/test/java/com/google/gplus/GooglePlusPersonSerDeTest.java
+++ streams-contrib/streams-provider-google/google-gplus/src/test/java/com/google/gplus/GooglePlusPersonSerDeTest.java
@@ -19,54 +19,55 @@
 
 package com.google.gplus;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.node.IntNode;
-import com.google.api.client.util.Lists;
 import com.google.api.services.plus.model.Person;
+import com.google.gplus.serializer.util.GPlusPersonDeserializer;
 import com.google.gplus.serializer.util.GooglePlusActivityUtil;
 import org.apache.commons.lang.StringUtils;
 import org.apache.streams.jackson.StreamsJacksonMapper;
 import org.apache.streams.pojo.json.Activity;
 import org.apache.streams.pojo.json.Actor;
 import org.apache.streams.pojo.json.Provider;
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class GooglePlusPersonSerDeTest {
     private final static Logger LOGGER = LoggerFactory.getLogger(GooglePlusPersonSerDeTest.class);
+    private ObjectMapper objectMapper;
+    private GooglePlusActivityUtil googlePlusActivityUtil;
 
-    @Test
-    public void TestPersonObjects() {
-        ObjectMapper objectMapper = new StreamsJacksonMapper();
+    @Before
+    public void setup() {
+        objectMapper = new StreamsJacksonMapper();
         SimpleModule simpleModule = new SimpleModule();
         simpleModule.addDeserializer(Person.class, new GPlusPersonDeserializer());
         objectMapper.registerModule(simpleModule);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
+        googlePlusActivityUtil = new GooglePlusActivityUtil();
+    }
+
+    @Test
+    public void TestPersonObjects() {
         InputStream is = GooglePlusPersonSerDeTest.class.getResourceAsStream("/google_plus_person_jsons.txt");
         InputStreamReader isr = new InputStreamReader(is);
         BufferedReader br = new BufferedReader(isr);
-
-        GooglePlusActivityUtil googlePlusActivityUtil = new GooglePlusActivityUtil();
 
         try {
             while (br.ready()) {
                 String line = br.readLine();
                 if (!StringUtils.isEmpty(line)) {
                     LOGGER.info("raw: {}", line);
-                    objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
                     Activity activity = new Activity();
 
                     Person person = objectMapper.readValue(line, Person.class);
@@ -92,48 +93,5 @@ public class GooglePlusPersonSerDeTest {
         } catch (Exception e) {
             LOGGER.error("Exception while testing serializability: {}", e);
         }
-    }
-}
-
-class GPlusPersonDeserializer extends JsonDeserializer<Person> {
-    @Override
-    public Person deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
-        ObjectMapper m = new StreamsJacksonMapper();
-
-        JsonNode node = jsonParser.getCodec().readTree(jsonParser);
-        Person person = new Person();
-
-        person.setCircledByCount((Integer)((IntNode)node.get("circledByCount")).numberValue());
-        person.setDisplayName(node.get("displayName").asText());
-        person.setEtag(node.get("etag").asText());
-        person.setGender(node.get("gender").asText());
-        person.setId(node.get("id").asText());
-
-        Person.Image image = new Person.Image();
-        JsonNode imageNode = node.get("image");
-        image.setIsDefault(imageNode.get("isDefault").asBoolean());
-        image.setUrl(imageNode.get("url").asText());
-        person.setImage(image);
-
-        person.setIsPlusUser(node.get("isPlusUser").asBoolean());
-        person.setKind(node.get("kind").asText());
-
-        JsonNode nameNode = node.get("name");
-        Person.Name name = m.readValue(m.writeValueAsString(nameNode), Person.Name.class);
-        person.setName(name);
-
-        person.setObjectType(node.get("objectType").asText());
-
-        List<Person.Organizations> organizations = Lists.newArrayList();
-        for(JsonNode orgNode : node.get("organizations")) {
-            Person.Organizations org = m.readValue(m.writeValueAsString(orgNode), Person.Organizations.class);
-            organizations.add(org);
-        }
-        person.setOrganizations(organizations);
-
-        person.setUrl(node.get("url").asText());
-        person.setVerified(node.get("verified").asBoolean());
-
-        return person;
     }
 }
