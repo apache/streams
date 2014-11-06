4e0e9dad415d1767f0f6191377066be11bc645dc
Ryan Ebanks <ryanebanks@gmail.com>
2014-11-06 17:08:34 -0600

    Fixed Gplus provider and added user data/activity collectors

---
+++ streams-contrib/streams-provider-google/google-gplus/src/main/java/com/google/gplus/provider/AbstractGPlusProvider.java
@@ -0,0 +1,234 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.google.gplus.provider;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.plus.Plus;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+import com.google.gson.Gson;
+import com.typesafe.config.Config;
+import org.apache.streams.config.StreamsConfigurator;
+import org.apache.streams.core.StreamsDatum;
+import org.apache.streams.core.StreamsProvider;
+import org.apache.streams.core.StreamsResultSet;
+import org.apache.streams.google.gplus.GPlusConfiguration;
+import org.apache.streams.google.gplus.configuration.UserInfo;
+import org.apache.streams.util.ComponentUtils;
+import org.apache.streams.util.api.requests.backoff.BackOffStrategy;
+import org.apache.streams.util.api.requests.backoff.impl.ExponentialBackOffStrategy;
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ *
+ */
+public abstract class AbstractGPlusProvider implements StreamsProvider {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(AbstractGPlusProvider.class);
+    private final static String SCOPE = "https://www.googleapis.com/auth/plus.stream.read";
+    private final static int MAX_BATCH_SIZE = 1000;
+
+    private static final HttpTransport TRANSPORT = new NetHttpTransport();
+    private static final JacksonFactory JSON_FACTORY = new JacksonFactory();
+    private static final Gson GSON = new Gson();
+
+    private GPlusConfiguration config;
+    private ExecutorService executor;
+    private BlockingQueue<StreamsDatum> datumQueue;
+    private BlockingQueue<Runnable> runnables;
+    private AtomicBoolean isComplete;
+    private boolean previousPullWasEmpty;
+
+    protected GoogleClientSecrets clientSecrets;
+    protected GoogleCredential credential;
+    protected Plus plus;
+
+
+
+    public AbstractGPlusProvider() {
+        Config config = StreamsConfigurator.config.getConfig("gplus");
+        this.config = GPlusConfigurator.detectConfiguration(config);
+    }
+
+    public AbstractGPlusProvider(GPlusConfiguration config) {
+        this.config = config;
+    }
+
+    @Override
+    public void startStream() {
+        BackOffStrategy backOffStrategy = new ExponentialBackOffStrategy(2);
+        for(UserInfo user : this.config.getGooglePlusUsers()) {
+            if(this.config.getDefaultAfterDate() != null && user.getAfterDate() == null) {
+                user.setAfterDate(this.config.getDefaultAfterDate());
+            }
+            if(this.config.getDefaultBeforeDate() != null && user.getBeforeDate() == null) {
+                user.setBeforeDate(this.config.getDefaultBeforeDate());
+            }
+            this.executor.submit(getDataCollector(backOffStrategy, this.datumQueue, this.plus, user));
+        }
+        this.executor.shutdown();
+    }
+
+    protected abstract Runnable getDataCollector(BackOffStrategy strategy, BlockingQueue<StreamsDatum> queue, Plus plus, UserInfo userInfo);
+
+    @Override
+    public StreamsResultSet readCurrent() {
+        BlockingQueue<StreamsDatum> batch = new LinkedBlockingQueue<>();
+        int batchCount = 0;
+        while(!this.datumQueue.isEmpty() && batchCount < MAX_BATCH_SIZE) {
+            StreamsDatum datum = ComponentUtils.pollWhileNotEmpty(this.datumQueue);
+            if(datum != null) {
+                ++batchCount;
+                ComponentUtils.offerUntilSuccess(datum, batch);
+            }
+        }
+        boolean pullIsEmpty = batch.isEmpty() && this.datumQueue.isEmpty() &&this.executor.isTerminated();
+        this.isComplete.set(this.previousPullWasEmpty && pullIsEmpty);
+        this.previousPullWasEmpty = pullIsEmpty;
+        return new StreamsResultSet(batch);
+    }
+
+    @Override
+    public StreamsResultSet readNew(BigInteger sequence) {
+        return null;
+    }
+
+    @Override
+    public StreamsResultSet readRange(DateTime start, DateTime end) {
+        return null;
+    }
+
+    @Override
+    public boolean isRunning() {
+        return !this.isComplete.get();
+    }
+
+    @Override
+    public void prepare(Object configurationObject) {
+
+        Preconditions.checkNotNull(config.getOauth().getConsumerKey());
+        Preconditions.checkNotNull(config.getOauth().getConsumerSecret());
+        Preconditions.checkNotNull(config.getOauth().getAccessToken());
+        Preconditions.checkNotNull(config.getOauth().getAccessTokenSecret());
+
+        try {
+            this.plus = createPlusClient();
+        } catch (IOException e) {
+            LOGGER.error("Failed to created oauth for GPlus : {}", e);
+            throw new RuntimeException(e);
+        }
+        // GPlus rate limits you to 5 calls per second, so there is not a need to execute more than one
+        // collector unless you have multiple oauth tokens
+        //TODO make this configurable based on the number of oauth tokens
+        this.executor = Executors.newFixedThreadPool(1);
+        this.datumQueue = new LinkedBlockingQueue<>(1000);
+        this.isComplete = new AtomicBoolean(false);
+        this.previousPullWasEmpty = false;
+    }
+
+    @VisibleForTesting
+    protected Plus createPlusClient() throws IOException{
+        credential = new GoogleCredential.Builder()
+                .setJsonFactory(JSON_FACTORY)
+                .setTransport(TRANSPORT)
+                .setClientSecrets(config.getOauth().getConsumerKey(), config.getOauth().getConsumerSecret()).build()
+                .setFromTokenResponse(JSON_FACTORY.fromString(
+                        config.getOauth().getAccessToken(), GoogleTokenResponse.class));
+        credential.refreshToken();
+        return new Plus.Builder(TRANSPORT,JSON_FACTORY, credential).build();
+    }
+
+    @Override
+    public void cleanUp() {
+        ComponentUtils.shutdownExecutor(this.executor, 10, 10);
+        this.executor = null;
+    }
+
+    public GPlusConfiguration getConfig() {
+        return config;
+    }
+
+    public void setConfig(GPlusConfiguration config) {
+        this.config = config;
+    }
+
+    /**
+     * Set and overwrite the default before date that was read from the configuration file.
+     * @param defaultBeforeDate
+     */
+    public void setDefaultBeforeDate(DateTime defaultBeforeDate) {
+        this.config.setDefaultBeforeDate(defaultBeforeDate);
+    }
+
+    /**
+     * Set and overwrite the default after date that was read from teh configuration file.
+     * @param defaultAfterDate
+     */
+    public void setDefaultAfterDate(DateTime defaultAfterDate) {
+        this.config.setDefaultAfterDate(defaultAfterDate);
+    }
+
+    /**
+     * Sets and overwrite the user info from the configuaration file.  Uses the defaults before and after dates.
+     * @param userIds
+     */
+    public void setUserInfoWithDefaultDates(Set<String> userIds) {
+        List<UserInfo> gPlusUsers = Lists.newLinkedList();
+        for(String userId : userIds) {
+            UserInfo user = new UserInfo();
+            user.setUserId(userId);
+            user.setAfterDate(this.config.getDefaultAfterDate());
+            user.setBeforeDate(this.config.getDefaultBeforeDate());
+            gPlusUsers.add(user);
+        }
+        this.config.setGooglePlusUsers(gPlusUsers);
+    }
+
+    /**
+     * Set and overwrite user into from teh configuration file. Only sets after dater.
+     * @param usersAndAfterDates
+     */
+    public void setUserInfoWithAfterDate(Map<String, DateTime> usersAndAfterDates) {
+        List<UserInfo> gPlusUsers = Lists.newLinkedList();
+        for(String userId : usersAndAfterDates.keySet()) {
+            UserInfo user = new UserInfo();
+            user.setUserId(userId);
+            user.setAfterDate(usersAndAfterDates.get(userId));
+            gPlusUsers.add(user);
+        }
+        this.config.setGooglePlusUsers(gPlusUsers);
+    }
+
+}
---
+++ streams-contrib/streams-provider-google/google-gplus/src/main/java/com/google/gplus/provider/GPlusUserDataCollector.java
@@ -0,0 +1,101 @@
+package com.google.gplus.provider;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.services.plus.Plus;
+import com.google.api.services.plus.model.Person;
+import com.google.gplus.serializer.util.GPlusPersonDeserializer;
+import org.apache.streams.core.StreamsDatum;
+import org.apache.streams.google.gplus.configuration.UserInfo;
+import org.apache.streams.jackson.StreamsJacksonMapper;
+import org.apache.streams.util.api.requests.backoff.BackOffStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.BlockingQueue;
+
+/**
+ *
+ */
+public  class GPlusUserDataCollector implements Runnable{
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GPlusUserDataCollector.class);
+    private static final ObjectMapper MAPPER = StreamsJacksonMapper.getInstance();
+    private static final int MAX_ATTEMPTS = 5;
+
+    static { //set up Mapper for Person objects
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addDeserializer(Person.class, new GPlusPersonDeserializer());
+        MAPPER.registerModule(simpleModule);
+        MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    private BackOffStrategy backOffStrategy;
+    private Plus gPlus;
+    private BlockingQueue<StreamsDatum> datumQueue;
+    private UserInfo userInfo;
+
+
+    public GPlusUserDataCollector(Plus gPlus, BackOffStrategy backOffStrategy, BlockingQueue<StreamsDatum> datumQueue, UserInfo userInfo) {
+        this.gPlus = gPlus;
+        this.backOffStrategy = backOffStrategy;
+        this.datumQueue = datumQueue;
+        this.userInfo = userInfo;
+    }
+
+    protected void queueUserHistory() {
+        try {
+            boolean tryAgain = false;
+            int attempts = 0;
+            com.google.api.services.plus.model.Person person = null;
+            do {
+                try {
+                    person = this.gPlus.people().get(userInfo.getUserId()).execute();
+                    this.backOffStrategy.reset();
+                    tryAgain = person == null;
+                } catch (GoogleJsonResponseException gjre) {
+                    switch (gjre.getStatusCode()) {
+                        case 400 :
+                            LOGGER.warn("Bad Request for user={} : {}", userInfo.getUserId(), gjre);
+                            tryAgain = false;
+                            break;
+                        case 401 :
+                            LOGGER.warn("Invalid Credentials : {}", gjre);
+                            tryAgain = false;
+                        case 403 :
+                            LOGGER.warn("Possible rate limit exception. Retrying. : {}", gjre.getMessage());
+                            this.backOffStrategy.backOff();
+                            tryAgain = true;
+                            break;
+                        case 503 :
+                            LOGGER.warn("Google Backend Service Error : {}", gjre);
+                            tryAgain = false;
+                            break;
+                        default:
+                            LOGGER.warn("Google Service returned error : {}", gjre);
+                            tryAgain = true;
+                            this.backOffStrategy.backOff();
+                            break;
+                    }
+                }
+                ++attempts;
+            } while(tryAgain && attempts < MAX_ATTEMPTS);
+            this.datumQueue.put(new StreamsDatum(MAPPER.writeValueAsString(person), person.getId()));
+        } catch (Throwable t) {
+            LOGGER.warn("Unable to pull user data for user={} : {}", userInfo.getUserId(), t);
+            if(t instanceof InterruptedException) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        queueUserHistory();
+    }
+
+
+
+}
