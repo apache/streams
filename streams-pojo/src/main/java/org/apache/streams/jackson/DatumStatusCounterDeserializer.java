8495725fdc2d07f4c5494632071b9378753487dc
Robert Douglas <rdouglas@w2ogroup.com>
2014-11-07 17:07:27 -0600

    STREAMS-210 | Initial commit with json schemas and skeleton for JMX bean message aggregation

---
+++ streams-pojo/src/main/java/org/apache/streams/jackson/DatumStatusCounterDeserializer.java
@@ -0,0 +1,7 @@
+package org.apache.streams.jackson;
+
+/**
+ * Created by rdouglas on 11/7/14.
+ */
+public class DatumStatusCounterDeserializer {
+}
---
+++ streams-runtimes/streams-runtime-local/src/main/java/org/apache/streams/local/counters/MemoryUsagePOJO.java
@@ -0,0 +1,7 @@
+package org.apache.streams.local.counters;
+
+/**
+ * Created by rdouglas on 11/7/14.
+ */
+public class MemoryUsagePOJO {
+}
