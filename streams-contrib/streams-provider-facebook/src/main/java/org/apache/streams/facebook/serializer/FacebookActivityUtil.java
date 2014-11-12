01efddab57355035ea480046d10122fa22e60eff
Robert Douglas <rdouglas@w2ogroup.com>
2014-11-12 12:29:55 -0600

    STREAMS-220 | Rewrote FacebookPostActivitySerializer to guarantee final Activity data shape

--- streams-contrib/streams-provider-facebook/src/main/java/org/apache/streams/facebook/serializer/FacebookActivityUtil.java
+++ streams-contrib/streams-provider-facebook/src/main/java/org/apache/streams/facebook/serializer/FacebookActivityUtil.java
@@ -15,27 +15,29 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 package org.apache.streams.facebook.serializer;
 
 import com.google.common.base.Joiner;
 import com.google.common.base.Optional;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import org.apache.streams.exceptions.ActivitySerializerException;
-import org.apache.streams.facebook.Cover;
-import org.apache.streams.facebook.Location;
-import org.apache.streams.facebook.Page;
-import org.apache.streams.pojo.json.Activity;
-import org.apache.streams.pojo.json.Actor;
-import org.apache.streams.pojo.json.Image;
-import org.apache.streams.pojo.json.Provider;
+import org.apache.streams.facebook.*;
+import org.apache.streams.facebook.Place;
+import org.apache.streams.facebook.Post;
+import org.apache.streams.pojo.json.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-import static org.apache.streams.data.util.ActivityUtil.ensureExtensions;
+import static org.apache.streams.data.util.ActivityUtil.*;
 
 public class FacebookActivityUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FacebookActivityUtil.class);
+
     /**
      * Updates the given Activity object with the values from the Page
      * @param page the object to use as the source
@@ -46,6 +48,61 @@ public class FacebookActivityUtil {
         activity.setActor(buildActor(page));
         activity.setId(null);
         activity.setProvider(getProvider());
+    }
+
+    /**
+     * Updates the given Activity object with the values from the Post
+     * @param post
+     * @param activity
+     * @throws ActivitySerializerException
+     */
+    public static void updateActivity(Post post, Activity activity) throws ActivitySerializerException {
+        activity.setActor(buildActor(post));
+        activity.setId(formatId(post.getId()));
+        activity.setProvider(getProvider());
+        activity.setUpdated(post.getUpdatedTime());
+        activity.setPublished(post.getCreatedTime());
+
+        if(post.getLink() != null && post.getLink().length() > 0) {
+            List<String> links = Lists.newArrayList();
+            links.add(post.getLink());
+            activity.setLinks(links);
+        }
+
+        activity.setContent(post.getMessage());
+
+        activity.setVerb("post");
+        activity.setObject(buildObject(post));
+        buildExtensions(activity, post);
+    }
+
+    /**
+     * Builds out the {@link org.apache.streams.pojo.json.ActivityObject} from the given {@link org.apache.streams.pojo.json.Post}
+     * @param post
+     * @return {@link org.apache.streams.pojo.json.ActivityObject}
+     */
+    public static ActivityObject buildObject(Post post) {
+        ActivityObject activityObject = new ActivityObject();
+
+        try {
+            activityObject.setContent(post.getMessage());
+            activityObject.setPublished(post.getCreatedTime());
+            activityObject.setUpdated(post.getUpdatedTime());
+            activityObject.setDisplayName(post.getFrom().getName());
+            activityObject.setId(formatId(post.getId()));
+            activityObject.setObjectType(post.getType());
+            activityObject.setUrl(post.getLink());
+
+            if(activityObject.getObjectType().equals("photo")) {
+                Image image = new Image();
+                image.setUrl(activityObject.getUrl());
+                activityObject.setImage(image);
+            }
+        } catch (Exception e) {
+            LOGGER.error("Exception while trying to build Activity object for post: {}, exception: {}", post, e);
+        }
+
+        return activityObject;
     }
 
     /**
@@ -95,6 +152,31 @@ public class FacebookActivityUtil {
     }
 
     /**
+     * Builds an {@link org.apache.streams.pojo.json.Actor} object from the {@link org.apache.streams.pojo.json.Post}
+     * @param post
+     * @return {@link org.apache.streams.pojo.json.Actor}
+     */
+    public static Actor buildActor(Post post) {
+        Actor actor = new Actor();
+
+        try {
+            actor.setId(formatId(
+                    Optional.fromNullable(
+                            post.getFrom().getId())
+                            .or(Optional.of(post.getFrom().getId().toString()))
+                            .orNull()
+            ));
+
+            actor.setDisplayName(post.getFrom().getName());
+            actor.setAdditionalProperty("handle", post.getFrom().getName());
+        } catch (Exception e) {
+            LOGGER.error("Exception trying to build actor for Post: {}, {}", post, e);
+        }
+
+        return actor;
+    }
+
+    /**
      * Builds the actor extensions given the page object
      * @param actor
      * @param page
@@ -112,6 +194,53 @@ public class FacebookActivityUtil {
         extensions.put("screenName", page.getUsername());
 
         actor.setAdditionalProperty("extensions", extensions);
+    }
+
+    /**
+     * Fills out the extensions attribute of the passed in {@link org.apache.streams.pojo.json.Activity}
+     * @param activity
+     * @param post
+     */
+    public static void buildExtensions(Activity activity, Post post) {
+        Map<String, Object> extensions = ensureExtensions(activity);
+
+        if(post.getLikes() != null) {
+            Map<String, Object> likes = Maps.newHashMap();
+            org.apache.streams.facebook.Like like = post.getLikes().get(0);
+
+            if(like.getAdditionalProperties().containsKey("data")) {
+                extensions.put("likes", likes);
+            }
+        }
+
+        if(post.getShares() != null) {
+            Map<String, Object> shares = Maps.newHashMap();
+            shares.put("count", ((Map<String, Object>)post.getShares()).get("count"));
+            extensions.put("rebroadcasts", shares);
+        }
+
+        if(post.getTo() != null) {
+            To to = post.getTo();
+            List<Datum> data = to.getData();
+            extensions.put("user_mentions", Lists.newArrayList());
+
+            for(Datum d : data) {
+                Map<String, String> mention = Maps.newHashMap();
+
+                mention.put("id", d.getId());
+                mention.put("displayName", d.getName());
+                mention.put("handle", d.getName());
+
+                ((List<Map<String,String>>)extensions.get("user_mentions")).add(mention);
+            }
+        }
+
+        if(post.getPlace() != null) {
+            Place place = post.getPlace();
+            if(place.getAdditionalProperties().containsKey("location")) {
+                extensions.put(LOCATION_EXTENSION, place.getAdditionalProperties().get("location"));
+            }
+        }
     }
 
     /**
