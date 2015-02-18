8dcfb6dc941f528f71e15af67017f8f3e4e747f1
sblackmon <sblackmon@apache.org>
2015-02-17 18:04:38 -0600

    added a test that tests compatibility of the generated verb beans and example activities.

--- streams-pojo/src/test/java/org/apache/streams/data/data/util/ActivitySerDeTest.java
+++ streams-pojo/src/test/java/org/apache/streams/data/data/util/ActivitySerDeTest.java
@@ -23,12 +23,18 @@ import org.apache.commons.io.Charsets;
 import org.apache.commons.io.IOUtils;
 import org.apache.streams.jackson.StreamsJacksonMapper;
 import org.apache.streams.pojo.json.Activity;
+import org.apache.streams.pojo.json.ActivityObject;
 import org.junit.Test;
+import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.InputStream;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Test that Activity beans are compatible with the example activities in the spec.
@@ -39,6 +45,10 @@ public class ActivitySerDeTest {
 
     private final static ObjectMapper MAPPER = StreamsJacksonMapper.getInstance();
 
+    /**
+     * Tests that all example activities can be loaded into Activity beans
+     * @throws Exception
+     */
     @Test
     public void testActivitySerDe() throws Exception {
 
@@ -47,13 +57,42 @@ public class ActivitySerDeTest {
         List<String> files = IOUtils.readLines(testActivityFolderStream, Charsets.UTF_8);
 
         for( String file : files) {
+            LOGGER.info("File: " + file );
             LOGGER.info("Serializing: activities/" + file );
             InputStream testActivityFileStream = ActivitySerDeTest.class.getClassLoader()
                     .getResourceAsStream("activities/" + file);
             Activity activity = MAPPER.readValue(testActivityFileStream, Activity.class);
             String activityString = MAPPER.writeValueAsString(activity);
             LOGGER.info("Deserialized: " + activityString );
+        }
+    }
 
+    /**
+     * Tests that defined activity verbs have an example which can be loaded into
+     * Activity beans and into verb-specific beans
+     * @throws Exception
+     */
+    @Test
+    public void testVerbSerDe() throws Exception {
+
+        Reflections reflections = new Reflections(new ConfigurationBuilder()
+                .setUrls(ClasspathHelper.forPackage("org.apache.streams.pojo.json"))
+                .setScanners(new SubTypesScanner()));
+        Set<Class<? extends Activity>> verbs = reflections.getSubTypesOf(Activity.class);
+
+        for( Class verbClass : verbs) {
+            LOGGER.info("Verb: " + verbClass.getSimpleName() );
+            Activity activity = (Activity) verbClass.newInstance();
+            String verbName = activity.getVerb();
+            String testfile = verbName.toLowerCase() + ".json";
+            LOGGER.info("Serializing: activities/" + testfile );
+            assert(ActivitySerDeTest.class.getClassLoader().getResource("activities/" + testfile) != null);
+            InputStream testActivityFileStream = ActivitySerDeTest.class.getClassLoader()
+                    .getResourceAsStream("activities/" + testfile);
+            assert(testActivityFileStream != null);
+            activity = MAPPER.convertValue(MAPPER.readValue(testActivityFileStream, verbClass), Activity.class);
+            String activityString = MAPPER.writeValueAsString(activity);
+            LOGGER.info("Deserialized: " + activityString );
         }
     }
 }
