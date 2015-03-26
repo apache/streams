c156fde7a87669454d724cb03a0fdc61f921f582
Robert Douglas <rdouglas@w2ogroup.com>
2015-03-26 17:04:08 -0500

    SBP-2125 | Ensuring that the raw Instagram object is placed into the activity's extensions map

--- streams-contrib/streams-provider-instagram/src/test/java/org/apache/streams/instagram/serializer/util/InstagramActivityUtilTest.java
+++ streams-contrib/streams-provider-instagram/src/test/java/org/apache/streams/instagram/serializer/util/InstagramActivityUtilTest.java
@@ -5,6 +5,8 @@ import org.jinstagram.entity.common.Comments;
 import org.jinstagram.entity.users.feed.MediaFeedData;
 import org.junit.Test;
 
+import java.util.Map;
+
 import static org.junit.Assert.assertTrue;
 
 public class InstagramActivityUtilTest {
@@ -25,5 +27,18 @@ public class InstagramActivityUtilTest {
         InstagramActivityUtil.addInstagramExtensions(activity, data);
 
         assertTrue(activity != null);
+    }
+
+    /**
+     * Ensures that we are adding the raw Instagram object into the activity's extensions
+     */
+    @Test
+    public void addInstagramExtensionTest() {
+        MediaFeedData data = new MediaFeedData();
+        Activity activity = new Activity();
+
+        InstagramActivityUtil.addInstagramExtensions(activity, data);
+
+        assertTrue(((Map<String, Object>)activity.getAdditionalProperties().get("extensions")).containsKey("instagram"));
     }
 }
