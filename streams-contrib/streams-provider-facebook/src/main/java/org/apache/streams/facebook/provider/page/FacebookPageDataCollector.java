1799fbfc7f64db653a97075e6ba8990e7c97eaa2
Steve Blackmon @steveblackmon <sblackmon@apache.org>
2016-11-14 23:26:18 +0100

    STREAMS-456: Facebook page provider does not include many fields

--- streams-contrib/streams-provider-facebook/src/main/java/org/apache/streams/facebook/provider/page/FacebookPageDataCollector.java
+++ streams-contrib/streams-provider-facebook/src/main/java/org/apache/streams/facebook/provider/page/FacebookPageDataCollector.java
@@ -15,69 +15,80 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package org.apache.streams.facebook.provider.page;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.annotations.VisibleForTesting;
-import facebook4j.*;
-import facebook4j.json.DataObjectFactory;
 import org.apache.streams.core.StreamsDatum;
 import org.apache.streams.facebook.FacebookConfiguration;
+import org.apache.streams.facebook.FacebookPageProviderConfiguration;
 import org.apache.streams.facebook.IdConfig;
 import org.apache.streams.facebook.provider.FacebookDataCollector;
 import org.apache.streams.jackson.StreamsJacksonMapper;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Joiner;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.BlockingQueue;
+
+import facebook4j.FacebookException;
+import facebook4j.Page;
+import facebook4j.Reading;
+import facebook4j.json.DataObjectFactory;
 
 /**
  * Collects the page data from public Facebook pages
  */
 public class FacebookPageDataCollector extends FacebookDataCollector {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(FacebookPageDataCollector.class);
-    private static final int MAX_ATTEMPTS = 5;
-    private static final ObjectMapper MAPPER = StreamsJacksonMapper.getInstance();
+  private static final Logger LOGGER = LoggerFactory.getLogger(FacebookPageDataCollector.class);
+  private static final int MAX_ATTEMPTS = 5;
+  private static final ObjectMapper MAPPER = StreamsJacksonMapper.getInstance();
 
-    public FacebookPageDataCollector(BlockingQueue<StreamsDatum> queue, FacebookConfiguration configuration) {
-        super(configuration, queue);
+  private String fields;
+
+  public FacebookPageDataCollector(BlockingQueue<StreamsDatum> queue, FacebookPageProviderConfiguration configuration) {
+    super(configuration, queue);
+    fields = Joiner.on(',').join(configuration.getFields());
+  }
+
+  @Override
+  protected void getData(IdConfig id) throws Exception {
+    Page responsePage = getPage(id.getId());
+    backOff.reset();
+
+    if (responsePage != null) {
+      super.outputData(MAPPER.readValue(DataObjectFactory.getRawJSON(responsePage), org.apache.streams.facebook.Page.class), responsePage.getId());
     }
+  }
 
-    @Override
-    protected void getData(IdConfig id) throws Exception {
-        Page responsePage = getPage(id.getId());
-        backOff.reset();
+  protected Page getPage(String pageId) throws Exception {
+    int attempt = 0;
+    while (attempt < MAX_ATTEMPTS) {
+      ++attempt;
+      try {
+        Page page = getNextFacebookClient().getPage(pageId, new Reading().fields(fields));
+        return page;
+      } catch (FacebookException fe) {
+        LOGGER.error("Facebook returned an exception : {}", fe);
+        LOGGER.error("Facebook returned an exception while trying to get feed for page, {} : {}", pageId, fe.getMessage());
 
-        if(responsePage != null) {
-            super.outputData(MAPPER.readValue(DataObjectFactory.getRawJSON(responsePage), org.apache.streams.facebook.Page.class), responsePage.getId());
+        int errorCode = fe.getErrorCode();
+
+        //Some sort of rate limiting
+        if (errorCode == 17 || errorCode == 4 || errorCode == 341) {
+          super.backOff.backOff();
         }
+      }
     }
+    throw new Exception("Failed to get data from facebook after " + MAX_ATTEMPTS);
+  }
 
-    protected Page getPage(String pageId) throws Exception {
-        int attempt = 0;
-        while(attempt < MAX_ATTEMPTS) {
-            ++attempt;
-            try {
-                Page page = getNextFacebookClient().getPage(pageId);
-                return page;
-            } catch (FacebookException fe) {
-                LOGGER.error("Facebook returned an exception : {}", fe);
-                LOGGER.error("Facebook returned an exception while trying to get feed for page, {} : {}", pageId, fe.getMessage());
-
-                int errorCode = fe.getErrorCode();
-
-                //Some sort of rate limiting
-                if(errorCode == 17 || errorCode == 4 || errorCode == 341) {
-                    super.backOff.backOff();
-                }
-            }
-        }
-        throw new Exception("Failed to get data from facebook after "+MAX_ATTEMPTS);
-    }
-
-    @VisibleForTesting
-    protected BlockingQueue<StreamsDatum> getQueue() {
-        return super.getQueue();
-    }
+  @VisibleForTesting
+  protected BlockingQueue<StreamsDatum> getQueue() {
+    return super.getQueue();
+  }
 }
--- streams-contrib/streams-provider-facebook/src/main/java/org/apache/streams/facebook/provider/page/FacebookPageProvider.java
+++ streams-contrib/streams-provider-facebook/src/main/java/org/apache/streams/facebook/provider/page/FacebookPageProvider.java
@@ -15,24 +15,29 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package org.apache.streams.facebook.provider.page;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Preconditions;
-import com.google.common.util.concurrent.Uninterruptibles;
-import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
-import com.typesafe.config.ConfigParseOptions;
 import org.apache.streams.config.ComponentConfigurator;
 import org.apache.streams.config.StreamsConfiguration;
 import org.apache.streams.config.StreamsConfigurator;
 import org.apache.streams.core.StreamsDatum;
 import org.apache.streams.facebook.FacebookConfiguration;
+import org.apache.streams.facebook.FacebookPageProviderConfiguration;
 import org.apache.streams.facebook.provider.FacebookDataCollector;
 import org.apache.streams.facebook.provider.FacebookProvider;
 import org.apache.streams.jackson.StreamsJacksonMapper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Joiner;
+import com.google.common.base.Preconditions;
+import com.google.common.util.concurrent.Uninterruptibles;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigParseOptions;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -48,77 +53,91 @@ import java.util.concurrent.TimeUnit;
  * Streams Provider which collects Page Profiles in the ID List contained in the
  * FacebookConfiguration object
  *
- * To use from command line:
+ * <p/>
+ * To use from command line,
  *
- *  Supply (at least) the following required configuration in application.conf:
+ * <p/>
+ * Launch using:
  *
- *  facebook.oauth.appId
- *  facebook.oauth.appSecret
- *  facebook.oauth.userAccessToken
+ * <p/>
+ * mvn exec:java -Dexec.mainClass=org.apache.streams.facebook.provider.page.FacebookPageProvider -Dexec.args="application.conf pages.json"
  *
- *  Launch using:
+ * <p/>
+ * Supply (at least) the following required configuration in application.conf:
  *
- *  mvn exec:java -Dexec.mainClass=org.apache.streams.facebook.provider.page.FacebookPageProvider -Dexec.args="application.conf pages.json"
-
+ * <p>
+ *   facebook.oauth.appId
+ *   facebook.oauth.appSecret
+ *   facebook.oauth.userAccessToken
+ * </p>
  */
 public class FacebookPageProvider extends FacebookProvider {
 
-    public static final String STREAMS_ID = "FacebookPageProvider";
+  public static final String STREAMS_ID = "FacebookPageProvider";
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(FacebookPageProvider.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(FacebookPageProvider.class);
 
-    private static ObjectMapper MAPPER = StreamsJacksonMapper.getInstance();
+  private static ObjectMapper MAPPER = StreamsJacksonMapper.getInstance();
 
-    public FacebookPageProvider(FacebookConfiguration facebookConfiguration) {
-        super(facebookConfiguration);
+  private FacebookPageProviderConfiguration configuration;
+
+  public FacebookPageProvider(FacebookPageProviderConfiguration facebookConfiguration) {
+    super(facebookConfiguration);
+    configuration = facebookConfiguration;
+  }
+
+  @VisibleForTesting
+  BlockingQueue<StreamsDatum> getQueue() {
+    return super.datums;
+  }
+
+  @Override
+  protected FacebookDataCollector getDataCollector() {
+    return new FacebookPageDataCollector(super.datums, configuration);
+  }
+
+  /**
+   * Run FacebookPageProvider from command line.
+   * @param args configfile outfile
+   * @throws Exception Exception
+   */
+  public static void main(String[] args) throws Exception {
+
+    Preconditions.checkArgument(args.length >= 2);
+
+    String configfile = args[0];
+    String outfile = args[1];
+
+    Config reference = ConfigFactory.load();
+    File confFile = new File(configfile);
+    assert (confFile.exists());
+    Config conf = ConfigFactory.parseFileAnySyntax(confFile, ConfigParseOptions.defaults().setAllowMissing(false));
+
+    Config typesafe  = conf.withFallback(reference).resolve();
+
+    StreamsConfiguration streamsConfiguration = StreamsConfigurator.detectConfiguration(typesafe);
+    FacebookPageProviderConfiguration config = new ComponentConfigurator<>(FacebookPageProviderConfiguration.class).detectConfiguration(typesafe, "facebook");
+    FacebookPageProvider provider = new FacebookPageProvider(config);
+
+    PrintStream outStream = new PrintStream(new BufferedOutputStream(new FileOutputStream(outfile)));
+    provider.prepare(config);
+    provider.startStream();
+    do {
+      Uninterruptibles.sleepUninterruptibly(streamsConfiguration.getBatchFrequencyMs(), TimeUnit.MILLISECONDS);
+      Iterator<StreamsDatum> iterator = provider.readCurrent().iterator();
+      while (iterator.hasNext()) {
+        StreamsDatum datum = iterator.next();
+        String json;
+        try {
+          json = MAPPER.writeValueAsString(datum.getDocument());
+          outStream.println(json);
+        } catch (JsonProcessingException ex) {
+          System.err.println(ex.getMessage());
+        }
+      }
     }
-
-    @VisibleForTesting
-    BlockingQueue<StreamsDatum> getQueue() {
-        return super.datums;
-    }
-
-    @Override
-    protected FacebookDataCollector getDataCollector() {
-        return new FacebookPageDataCollector(super.datums, super.configuration);
-    }
-
-    public static void main(String[] args) throws Exception {
-
-        Preconditions.checkArgument(args.length >= 2);
-
-        String configfile = args[0];
-        String outfile = args[1];
-
-        Config reference = ConfigFactory.load();
-        File conf_file = new File(configfile);
-        assert(conf_file.exists());
-        Config conf = ConfigFactory.parseFileAnySyntax(conf_file, ConfigParseOptions.defaults().setAllowMissing(false));
-
-        Config typesafe  = conf.withFallback(reference).resolve();
-
-        StreamsConfiguration streamsConfiguration = StreamsConfigurator.detectConfiguration(typesafe);
-        FacebookConfiguration config = new ComponentConfigurator<>(FacebookConfiguration.class).detectConfiguration(typesafe, "facebook");
-        FacebookPageProvider provider = new FacebookPageProvider(config);
-
-        PrintStream outStream = new PrintStream(new BufferedOutputStream(new FileOutputStream(outfile)));
-        provider.prepare(config);
-        provider.startStream();
-        do {
-            Uninterruptibles.sleepUninterruptibly(streamsConfiguration.getBatchFrequencyMs(), TimeUnit.MILLISECONDS);
-            Iterator<StreamsDatum> iterator = provider.readCurrent().iterator();
-            while(iterator.hasNext()) {
-                StreamsDatum datum = iterator.next();
-                String json;
-                try {
-                    json = MAPPER.writeValueAsString(datum.getDocument());
-                    outStream.println(json);
-                } catch (JsonProcessingException e) {
-                    System.err.println(e.getMessage());
-                }
-            }
-        } while( provider.isRunning());
-        provider.cleanUp();
-        outStream.flush();
-    }
+    while ( provider.isRunning());
+    provider.cleanUp();
+    outStream.flush();
+  }
 }
