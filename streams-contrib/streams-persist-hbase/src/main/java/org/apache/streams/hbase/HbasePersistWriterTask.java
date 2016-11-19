7e2b49f32ff11d0b6b5f06b1e22385a8c4fedf22
Steve Blackmon @steveblackmon <sblackmon@apache.org>
2016-11-19 18:34:41 +0100

    STREAMS-440: streams-persist-hbase
     reduce wc -l target/checkstyle-result.xml from 204 to 20

--- streams-contrib/streams-persist-hbase/src/main/java/org/apache/streams/hbase/HbasePersistWriterTask.java
+++ streams-contrib/streams-persist-hbase/src/main/java/org/apache/streams/hbase/HbasePersistWriterTask.java
@@ -19,38 +19,45 @@
 package org.apache.streams.hbase;
 
 import org.apache.streams.core.StreamsDatum;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Random;
 
+/**
+ * HbasePersistWriterTask writes to hbase on behalf of
+ * @see org.apache.streams.hbase.HbasePersistWriter
+ */
 public class HbasePersistWriterTask implements Runnable {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(HbasePersistWriterTask.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(HbasePersistWriterTask.class);
 
-    private HbasePersistWriter writer;
+  private HbasePersistWriter writer;
 
-    public HbasePersistWriterTask(HbasePersistWriter writer) {
-        this.writer = writer;
-    }
+  public HbasePersistWriterTask(HbasePersistWriter writer) {
+    this.writer = writer;
+  }
 
-    @Override
-    public void run() {
+  @Override
+  public void run() {
 
-        while(true) {
-            if( writer.persistQueue.peek() != null ) {
-                try {
-                    StreamsDatum entry = writer.persistQueue.remove();
-                    writer.write(entry);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-            try {
-                Thread.sleep(new Random().nextInt(1));
-            } catch (InterruptedException ignored) {}
+    while (true) {
+      if ( writer.persistQueue.peek() != null ) {
+        try {
+          StreamsDatum entry = writer.persistQueue.remove();
+          writer.write(entry);
+        } catch (Exception ex) {
+          ex.printStackTrace();
         }
-
+      }
+      try {
+        Thread.sleep(new Random().nextInt(1));
+      } catch (InterruptedException ignored) {
+        LOGGER.trace("ignored InterruptedException", ignored);
+      }
     }
+
+  }
 
 }
