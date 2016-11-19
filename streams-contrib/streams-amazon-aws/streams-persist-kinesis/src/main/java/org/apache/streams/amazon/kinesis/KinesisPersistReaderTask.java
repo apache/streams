db47e801b2901d7b1720cf6dc45646bcf5373dae
Steve Blackmon @steveblackmon <sblackmon@apache.org>
2016-11-19 18:26:49 +0100

    STREAMS-440: streams-persist-kinesis
    decrease wc -l checkstyle-result.xml from 222 to 64

--- streams-contrib/streams-amazon-aws/streams-persist-kinesis/src/main/java/org/apache/streams/amazon/kinesis/KinesisPersistReaderTask.java
+++ streams-contrib/streams-amazon-aws/streams-persist-kinesis/src/main/java/org/apache/streams/amazon/kinesis/KinesisPersistReaderTask.java
@@ -18,94 +18,102 @@
 
 package org.apache.streams.amazon.kinesis;
 
+import org.apache.streams.config.StreamsConfigurator;
+import org.apache.streams.core.StreamsDatum;
+
 import com.amazonaws.services.kinesis.model.GetRecordsRequest;
 import com.amazonaws.services.kinesis.model.GetRecordsResult;
 import com.amazonaws.services.kinesis.model.GetShardIteratorRequest;
 import com.amazonaws.services.kinesis.model.GetShardIteratorResult;
 import com.amazonaws.services.kinesis.model.Record;
-import com.amazonaws.util.Base64;
 import com.google.common.collect.Maps;
-import org.apache.streams.config.StreamsConfigurator;
-import org.apache.streams.core.StreamsDatum;
+
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.math.BigInteger;
 import java.nio.charset.Charset;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
+/**
+ * KinesisPersistReaderTask reads documents from kinesis on behalf of
+ * @see {@link KinesisPersistReader}.
+ */
 public class KinesisPersistReaderTask implements Runnable {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(KinesisPersistReaderTask.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(KinesisPersistReaderTask.class);
 
-    private KinesisPersistReader reader;
-    private String streamName;
-    private String shardId;
+  private KinesisPersistReader reader;
+  private String streamName;
+  private String shardId;
 
-    private String shardIteratorId;
+  private String shardIteratorId;
 
-    private Long pollInterval = StreamsConfigurator.detectConfiguration().getBatchFrequencyMs();
+  private Long pollInterval = StreamsConfigurator.detectConfiguration().getBatchFrequencyMs();
 
-    public KinesisPersistReaderTask(KinesisPersistReader reader, String streamName, String shardId) {
-        this.reader = reader;
-        this.streamName = streamName;
-        this.shardId = shardId;
-    }
+  /**
+   * KinesisPersistReaderTask constructor.
+   */
+  public KinesisPersistReaderTask(KinesisPersistReader reader, String streamName, String shardId) {
+    this.reader = reader;
+    this.streamName = streamName;
+    this.shardId = shardId;
+  }
 
-    @Override
-    public void run() {
+  @Override
+  public void run() {
 
-        GetShardIteratorRequest shardIteratorRequest = new GetShardIteratorRequest()
-                .withStreamName(this.streamName)
-                .withShardId(shardId)
-                .withShardIteratorType("TRIM_HORIZON");
+    GetShardIteratorRequest shardIteratorRequest = new GetShardIteratorRequest()
+        .withStreamName(this.streamName)
+        .withShardId(shardId)
+        .withShardIteratorType("TRIM_HORIZON");
 
-        GetShardIteratorResult shardIteratorResult = reader.client.getShardIterator(shardIteratorRequest);
+    GetShardIteratorResult shardIteratorResult = reader.client.getShardIterator(shardIteratorRequest);
 
-        shardIteratorId = shardIteratorResult.getShardIterator();
+    shardIteratorId = shardIteratorResult.getShardIterator();
 
-        Map<String,Object> metadata = Maps.newHashMap();
-        metadata.put("streamName", streamName);
-        metadata.put("shardId", shardId);
+    Map<String,Object> metadata = Maps.newHashMap();
+    metadata.put("streamName", streamName);
+    metadata.put("shardId", shardId);
 
-        while(true) {
+    while (true) {
 
-            GetRecordsRequest recordsRequest = new GetRecordsRequest()
-                    .withShardIterator(shardIteratorId);
+      GetRecordsRequest recordsRequest = new GetRecordsRequest()
+          .withShardIterator(shardIteratorId);
 
-            GetRecordsResult recordsResult = reader.client.getRecords(recordsRequest);
+      GetRecordsResult recordsResult = reader.client.getRecords(recordsRequest);
 
-            LOGGER.info("{} records {} millis behind {}:{}:{} ", recordsResult.getRecords().size(), recordsResult.getMillisBehindLatest(), streamName, shardId, shardIteratorId);
+      LOGGER.info("{} records {} millis behind {}:{}:{} ", recordsResult.getRecords().size(), recordsResult.getMillisBehindLatest(), streamName, shardId, shardIteratorId);
 
-            shardIteratorId = recordsResult.getNextShardIterator();
+      shardIteratorId = recordsResult.getNextShardIterator();
 
-            List<Record> recordList = recordsResult.getRecords();
+      List<Record> recordList = recordsResult.getRecords();
 
-            for (Record record : recordList) {
-                try {
-                    byte[] byteArray = record.getData().array();
-                    //byte[] decoded = Base64.decode(byteArray);
-                    String message = new String(byteArray, Charset.forName("UTF-8"));
-                    reader.persistQueue.add(
-                            new StreamsDatum(
-                                    message,
-                                    record.getPartitionKey(),
-                                    new DateTime(),
-                                    new BigInteger(record.getSequenceNumber()),
-                                    metadata));
-                } catch( Exception e ) {
-                    LOGGER.warn("Exception processing record {}: {}", record, e);
-                }
-            }
-            try {
-                Thread.sleep(reader.pollInterval);
-            } catch (InterruptedException e) {}
+      for (Record record : recordList) {
+        try {
+          byte[] byteArray = record.getData().array();
+          //byte[] decoded = Base64.decode(byteArray);
+          String message = new String(byteArray, Charset.forName("UTF-8"));
+          reader.persistQueue.add(
+              new StreamsDatum(
+                  message,
+                  record.getPartitionKey(),
+                  new DateTime(),
+                  new BigInteger(record.getSequenceNumber()),
+                  metadata));
+        } catch ( Exception ex ) {
+          LOGGER.warn("Exception processing record {}: {}", record, ex);
         }
-
+      }
+      try {
+        Thread.sleep(reader.pollInterval);
+      } catch (InterruptedException ex) {
+        LOGGER.trace("InterruptedException", ex);
+      }
     }
+
+  }
 
 }
