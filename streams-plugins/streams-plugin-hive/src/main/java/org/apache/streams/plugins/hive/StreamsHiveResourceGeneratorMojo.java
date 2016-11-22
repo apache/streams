54232cd7a272f72a72f5f1ea27ffb429d0d16e8c
Steve Blackmon @steveblackmon <sblackmon@apache.org>
2016-11-22 15:30:08 -0600

    STREAMS-440: streams-plugin-hive
    reduce wc -l target/checkstyle_result.xml from 442 to 20

--- streams-plugins/streams-plugin-hive/src/main/java/org/apache/streams/plugins/hive/StreamsHiveResourceGeneratorMojo.java
+++ streams-plugins/streams-plugin-hive/src/main/java/org/apache/streams/plugins/hive/StreamsHiveResourceGeneratorMojo.java
@@ -34,49 +34,57 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.util.List;
 
-@Mojo(  name = "generate-resources",
-        defaultPhase = LifecyclePhase.GENERATE_RESOURCES
-)
-@Execute(   goal = "generate-resources",
-            phase = LifecyclePhase.GENERATE_RESOURCES
-)
+@Mojo (
+    name = "generate-resources",
+    defaultPhase = LifecyclePhase.GENERATE_RESOURCES
+    )
+@Execute (
+    goal = "generate-resources",
+    phase = LifecyclePhase.GENERATE_RESOURCES
+    )
 public class StreamsHiveResourceGeneratorMojo extends AbstractMojo {
 
-    private final static Logger LOGGER = LoggerFactory.getLogger(StreamsHiveResourceGeneratorMojo.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(StreamsHiveResourceGeneratorMojo.class);
 
-    private volatile MojoFailureException mojoFailureException;
+  private volatile MojoFailureException mojoFailureException;
 
-    @Component
-    private MavenProject project;
+  @Component
+  private MavenProject project;
 
-    @Parameter( defaultValue = "${project.basedir}", readonly = true )
-    private File basedir;
+  @Parameter( defaultValue = "${project.basedir}", readonly = true )
+  private File basedir;
 
-    @Parameter( defaultValue = "src/main/jsonschema", readonly = true ) // Maven 3 only
-    public String sourceDirectory;
+  @Parameter( defaultValue = "src/main/jsonschema", readonly = true ) // Maven 3 only
+  public String sourceDirectory;
 
-    @Parameter( readonly = true ) // Maven 3 only
-    public List<String> sourcePaths;
+  @Parameter( readonly = true ) // Maven 3 only
+  public List<String> sourcePaths;
 
-    @Parameter(defaultValue = "./target/generated-resources/hive", readonly = true)
-    public String targetDirectory;
+  @Parameter(defaultValue = "./target/generated-resources/hive", readonly = true)
+  public String targetDirectory;
 
-    public void execute() throws MojoExecutionException, MojoFailureException {
+  /**
+   * execute StreamsHiveResourceGeneratorMojo.
+   * @throws MojoExecutionException MojoExecutionException
+   * @throws MojoFailureException MojoFailureException
+   */
+  public void execute() throws MojoExecutionException, MojoFailureException {
 
-        //addProjectDependenciesToClasspath();
+    //addProjectDependenciesToClasspath();
 
-        StreamsHiveGenerationConfig config = new StreamsHiveGenerationConfig();
+    StreamsHiveGenerationConfig config = new StreamsHiveGenerationConfig();
 
-        if( sourcePaths != null && sourcePaths.size() > 0)
-            config.setSourcePaths(sourcePaths);
-        else
-            config.setSourceDirectory(sourceDirectory);
-        config.setTargetDirectory(targetDirectory);
-
-        StreamsHiveResourceGenerator streamsHiveResourceGenerator = new StreamsHiveResourceGenerator(config);
-
-        streamsHiveResourceGenerator.run();
-
+    if ( sourcePaths != null && sourcePaths.size() > 0) {
+      config.setSourcePaths(sourcePaths);
+    } else {
+      config.setSourceDirectory(sourceDirectory);
     }
+    config.setTargetDirectory(targetDirectory);
+
+    StreamsHiveResourceGenerator streamsHiveResourceGenerator = new StreamsHiveResourceGenerator(config);
+
+    streamsHiveResourceGenerator.run();
+
+  }
 
 }
