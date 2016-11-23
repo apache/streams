262657144cc57c1893f93b4340a8f791cd1a56c4
Steve Blackmon @steveblackmon <sblackmon@apache.org>
2016-11-22 18:23:07 -0600

    STREAMS-440: streams-runtime-dropwizard
    reduce wc -l target/checkstyle_result.xml from 311 to 19

--- streams-runtimes/streams-runtime-dropwizard/src/main/java/org/apache/streams/dropwizard/StreamDropwizardBuilder.java
+++ streams-runtimes/streams-runtime-dropwizard/src/main/java/org/apache/streams/dropwizard/StreamDropwizardBuilder.java
@@ -18,46 +18,47 @@
 
 package org.apache.streams.dropwizard;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.streams.config.StreamsConfiguration;
-import org.apache.streams.config.StreamsConfigurator;
 import org.apache.streams.core.StreamBuilder;
 import org.apache.streams.core.StreamsProvider;
 import org.apache.streams.local.LocalRuntimeConfiguration;
 import org.apache.streams.local.builders.LocalStreamBuilder;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.util.Map;
 
 /**
- * StreamDropwizardBuilder is currently a light wrapper around LocalStreamBuilder
+ * StreamDropwizardBuilder is currently a light wrapper around LocalStreamBuilder.
  *
+ * <p/>
  * It's a seperate class because they will almost certainly deviate going forward
  */
 public class StreamDropwizardBuilder extends LocalStreamBuilder implements StreamBuilder {
 
-    public StreamDropwizardBuilder() {
-        super();
-    }
+  public StreamDropwizardBuilder() {
+    super();
+  }
 
-    public StreamDropwizardBuilder(StreamsConfiguration streamConfig) {
-        super(new ObjectMapper().convertValue(streamConfig, LocalRuntimeConfiguration.class));
-    }
+  public StreamDropwizardBuilder(StreamsConfiguration streamConfig) {
+    super(new ObjectMapper().convertValue(streamConfig, LocalRuntimeConfiguration.class));
+  }
 
-    public StreamDropwizardBuilder(Map<String, Object> streamConfig) {
-        super(streamConfig);
-    }
+  public StreamDropwizardBuilder(Map<String, Object> streamConfig) {
+    super(streamConfig);
+  }
 
-    public StreamDropwizardBuilder(int maxQueueCapacity) {
-        super(maxQueueCapacity);
-    }
+  public StreamDropwizardBuilder(int maxQueueCapacity) {
+    super(maxQueueCapacity);
+  }
 
-    public StreamDropwizardBuilder(int maxQueueCapacity, Map<String, Object> streamConfig) {
-        super(maxQueueCapacity, streamConfig);
-    }
+  public StreamDropwizardBuilder(int maxQueueCapacity, Map<String, Object> streamConfig) {
+    super(maxQueueCapacity, streamConfig);
+  }
 
-    @Override
-    public StreamBuilder newPerpetualStream(String streamId, StreamsProvider provider) {
-        return super.newPerpetualStream(streamId, provider);
-    }
+  @Override
+  public StreamBuilder newPerpetualStream(String streamId, StreamsProvider provider) {
+    return super.newPerpetualStream(streamId, provider);
+  }
 
 }
--- streams-runtimes/streams-runtime-dropwizard/src/main/java/org/apache/streams/dropwizard/StreamsDropwizardModule.java
+++ streams-runtimes/streams-runtime-dropwizard/src/main/java/org/apache/streams/dropwizard/StreamsDropwizardModule.java
@@ -18,35 +18,31 @@
 
 package org.apache.streams.dropwizard;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.google.inject.AbstractModule;
-import com.google.inject.Provides;
-import com.google.inject.Singleton;
-import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
-import com.typesafe.config.ConfigRenderOptions;
 import org.apache.streams.config.StreamsConfiguration;
 import org.apache.streams.config.StreamsConfigurator;
 
-import java.io.IOException;
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
 
 /**
  * This class exists because dropwizard-guice requires at least
- * one module to run
+ * one module to run.
  *
+ * <p/>
  * Do not expect @Inject StreamsConfiguration to work at the moment.
  */
 public class StreamsDropwizardModule extends AbstractModule {
 
-    @Override
-    protected void configure() {
-        requestStaticInjection(StreamsConfiguration.class);
-    }
+  @Override
+  protected void configure() {
+    requestStaticInjection(StreamsConfiguration.class);
+  }
 
-    @Provides
-    @Singleton
-    public StreamsConfiguration providesStreamsConfiguration() {
-        return StreamsConfigurator.detectConfiguration();
-    }
+  @Provides
+  @Singleton
+  public StreamsConfiguration providesStreamsConfiguration() {
+    return StreamsConfigurator.detectConfiguration();
+  }
 
 }
