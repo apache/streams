86890fc0f82106093010f1971175ed54179a0c58
Steve Blackmon @steveblackmon <sblackmon@apache.org>
2016-11-22 18:13:35 -0600

    STREAMS-440: streams-pojo-extensions
    reduce wc -l target/checkstyle_result.xml from 230 to 16

--- streams-pojo-extensions/src/main/java/org/apache/streams/data/util/PropertyUtil.java
+++ streams-pojo-extensions/src/main/java/org/apache/streams/data/util/PropertyUtil.java
@@ -18,17 +18,17 @@
 
 package org.apache.streams.data.util;
 
+import org.apache.streams.jackson.StreamsJacksonMapper;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.node.ValueNode;
-import com.google.common.base.Joiner;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
-import org.apache.streams.jackson.StreamsJacksonMapper;
 
 import java.util.Iterator;
 import java.util.List;
@@ -39,87 +39,88 @@ import java.util.Map;
  */
 public class PropertyUtil {
 
-    private static final ObjectMapper mapper = StreamsJacksonMapper.getInstance();
+  private static final ObjectMapper mapper = StreamsJacksonMapper.getInstance();
 
-    public static Map<String, Object> flattenToMap(ObjectNode object) {
-        Map<String, Object> flatObject = Maps.newHashMap();
-        addKeys(new String(), object, flatObject, '.');
-        return flatObject;
+  public static Map<String, Object> flattenToMap(ObjectNode object) {
+    Map<String, Object> flatObject = Maps.newHashMap();
+    addKeys(new String(), object, flatObject, '.');
+    return flatObject;
+  }
+
+  public static Map<String, Object> flattenToMap(ObjectNode object, char seperator) {
+    Map<String, Object> flatObject = Maps.newHashMap();
+    addKeys(new String(), object, flatObject, seperator);
+    return flatObject;
+  }
+
+  public static ObjectNode flattenToObjectNode(ObjectNode object) {
+    Map<String, Object> flatObject = flattenToMap(object, '.');
+    addKeys(new String(), object, flatObject, '.');
+    return mapper.convertValue(flatObject, ObjectNode.class);
+  }
+
+  public static ObjectNode flattenToObjectNode(ObjectNode object, char seperator) {
+    Map<String, Object> flatObject = flattenToMap(object, seperator);
+    addKeys(new String(), object, flatObject, seperator);
+    return mapper.convertValue(flatObject, ObjectNode.class);
+  }
+
+  private static void addKeys(String currentPath, JsonNode jsonNode, Map<String, Object> map, char seperator) {
+    if (jsonNode.isObject()) {
+      ObjectNode objectNode = (ObjectNode) jsonNode;
+      Iterator<Map.Entry<String, JsonNode>> iter = objectNode.fields();
+      String pathPrefix = currentPath.isEmpty() ? "" : currentPath + seperator;
+
+      while (iter.hasNext()) {
+        Map.Entry<String, JsonNode> entry = iter.next();
+        addKeys(pathPrefix + entry.getKey(), entry.getValue(), map, seperator);
+      }
+    } else if (jsonNode.isArray()) {
+      ArrayNode arrayNode = (ArrayNode) jsonNode;
+      map.put(currentPath, arrayNode);
+    } else if (jsonNode.isValueNode()) {
+      ValueNode valueNode = (ValueNode) jsonNode;
+      if ( valueNode.isTextual() ) {
+        map.put(currentPath, valueNode.asText());
+      } else if ( valueNode.isNumber() ) {
+        map.put(currentPath, valueNode);
+      }
     }
+  }
 
-    public static ObjectNode flattenToObjectNode(ObjectNode object) {
-        Map<String, Object> flatObject = flattenToMap(object, '.');
-        addKeys(new String(), object, flatObject, '.');
-        return mapper.convertValue(flatObject, ObjectNode.class);
-    }
+  public static ObjectNode unflattenMap(Map<String, Object> object, char seperator) {
+    return unflattenObjectNode(mapper.convertValue(object, ObjectNode.class), seperator);
+  }
 
-    public static Map<String, Object> flattenToMap(ObjectNode object, char seperator) {
-        Map<String, Object> flatObject = Maps.newHashMap();
-        addKeys(new String(), object, flatObject, seperator);
-        return flatObject;
-    }
-
-    public static ObjectNode flattenToObjectNode(ObjectNode object, char seperator) {
-        Map<String, Object> flatObject = flattenToMap(object, seperator);
-        addKeys(new String(), object, flatObject, seperator);
-        return mapper.convertValue(flatObject, ObjectNode.class);
-    }
-
-    private static void addKeys(String currentPath, JsonNode jsonNode, Map<String, Object> map, char seperator) {
-        if (jsonNode.isObject()) {
-            ObjectNode objectNode = (ObjectNode) jsonNode;
-            Iterator<Map.Entry<String, JsonNode>> iter = objectNode.fields();
-            String pathPrefix = currentPath.isEmpty() ? "" : currentPath + seperator;
-
-            while (iter.hasNext()) {
-                Map.Entry<String, JsonNode> entry = iter.next();
-                addKeys(pathPrefix + entry.getKey(), entry.getValue(), map, seperator);
-            }
-        } else if (jsonNode.isArray()) {
-            ArrayNode arrayNode = (ArrayNode) jsonNode;
-            map.put(currentPath, arrayNode);
-        } else if (jsonNode.isValueNode()) {
-            ValueNode valueNode = (ValueNode) jsonNode;
-            if( valueNode.isTextual() )
-                map.put(currentPath, valueNode.asText());
-            else if ( valueNode.isNumber() )
-                map.put(currentPath, valueNode);
+  public static ObjectNode unflattenObjectNode(ObjectNode flatObject, char seperator) {
+    ObjectNode root = mapper.createObjectNode();
+    Iterator<Map.Entry<String, JsonNode>> iter = flatObject.fields();
+    while (iter.hasNext()) {
+      Map.Entry<String, JsonNode> item = iter.next();
+      String fullKey = item.getKey();
+      if ( !fullKey.contains(Character.valueOf(seperator).toString())) {
+        root.put(item.getKey(), item.getValue());
+      } else {
+        ObjectNode currentNode = root;
+        List<String> keyParts = Lists.newArrayList();
+        Iterables.addAll(keyParts, Splitter.on(seperator).split(item.getKey()));
+        Iterator<String> keyPartIterator = Iterables.limit(Splitter.on(seperator).split(item.getKey()), keyParts.size() - 1).iterator();
+        while ( keyPartIterator.hasNext()) {
+          String part = keyPartIterator.next();
+          if ( currentNode.has(part) && currentNode.get(part).isObject() ) {
+            currentNode = (ObjectNode) currentNode.get(part);
+          } else {
+            ObjectNode newNode = mapper.createObjectNode();
+            currentNode.put(part, newNode);
+            currentNode = newNode;
+          }
         }
-    }
+        currentNode.put(keyParts.get(keyParts.size() - 1), item.getValue());
 
-    public static ObjectNode unflattenMap(Map<String, Object> object, char seperator) {
-        return unflattenObjectNode(mapper.convertValue(object, ObjectNode.class), seperator);
+      }
     }
-
-    public static ObjectNode unflattenObjectNode(ObjectNode flatObject, char seperator) {
-        ObjectNode root = mapper.createObjectNode();
-        Iterator<Map.Entry<String, JsonNode>> iter = flatObject.fields();
-        while (iter.hasNext()) {
-            Map.Entry<String, JsonNode> item = iter.next();
-            String fullKey = item.getKey();
-            if( !fullKey.contains(Character.valueOf(seperator).toString())) {
-                root.put(item.getKey(), item.getValue());
-            } else {
-                ObjectNode currentNode = root;
-                List<String> keyParts = Lists.newArrayList();
-                Iterables.addAll(keyParts, Splitter.on(seperator).split(item.getKey()));
-                Iterator<String> keyPartIterator = Iterables.limit(Splitter.on(seperator).split(item.getKey()), keyParts.size()-1).iterator();
-                while( keyPartIterator.hasNext()) {
-                    String part = keyPartIterator.next();
-                    if( currentNode.has(part) && currentNode.get(part).isObject() ) {
-                        currentNode = (ObjectNode) currentNode.get(part);
-                    } else {
-                        ObjectNode newNode = mapper.createObjectNode();
-                        currentNode.put(part, newNode);
-                        currentNode = newNode;
-                    }
-                };
-                currentNode.put(keyParts.get(keyParts.size()-1), item.getValue());
-
-            }
-        }
-        return root;
-    }
+    return root;
+  }
 
 
 }
