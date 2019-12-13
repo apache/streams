2a3ee1248cc6cfa18311f7cb8935fdb5030d4ed5
sblackmon <sblackmon@apache.org>
2019-12-13 16:34:52 -0600

    first commit streams-processor-pipl

---
+++ streams-contrib/streams-processor-pipl/src/main/java/org/apache/streams/pipl/Search.java
@@ -0,0 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.streams.pipl;
+
+import org.apache.juneau.http.annotation.Body;
+import org.apache.juneau.remote.RemoteInterface;
+import org.apache.juneau.rest.client.remote.RemoteMethod;
+import org.apache.streams.pipl.api.SearchRequest;
+import org.apache.streams.pipl.api.SearchResponse;
+
+@RemoteInterface(path = "https://api.pipl.com/search")
+public interface Search {
+
+  @RemoteMethod(method ="POST")
+  public SearchResponse search(@Body SearchRequest request);
+
+}
